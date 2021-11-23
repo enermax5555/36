d656511bea26fd5c296434a9cefed8508c5629cd
Стоян Калпакчиев <stoqn.sisimen@gmail.com>
2021-11-23 12:35:04 -0300

    Fix the args for render()

--- src/js/Notification.js
+++ src/js/Notification.js
@@ -14,7 +14,7 @@ export default class Notification {
     this.container.classList.add("notification-container");
   }
 
-  render(type, price) {
+  render({type, price}) {
     const template = `
 <div class="notification type-${type}">
   <button class="delete"></button>
